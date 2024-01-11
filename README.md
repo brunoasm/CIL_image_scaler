635ea22bbc3caf3cfcea6916267d097e6b634633
Bruno de Medeiros <bdemedeiros@fieldmuseum.org>
2024-01-11 16:25:35 -0600

    fix git

--- README.md
+++
@@ -1,4 +0,0 @@
-Usage:
-```
-Rscript -e 'if (!require("shiny")) install.packages("shiny"); library(shiny); runGitHub("brunoasm/CIL_image_scaler", launch.browser = T)'
-```
--- scale_bar_gui.py
+++ scale_bar_gui.py
@@ -3,21 +3,42 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
-import os, sys
+import os
 
 # Function definitions (including the provided add_scale_bar function) go here
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
-
-csv_path = resource_path('scale_info.csv')
-
-
 # Function to read and process the scale_info.csv file
 def read_scale_info(csv_path):
-    scale_info = pd.read_csv(csv_path)
+    scale_info = pd.DataFrame([{'station': 'green', 'scale_file': 'NikonD5100_10x_P1.tif', 'camera': 'Nikon', 'match_pattern': '10x_P1', 'pixels_per_mm': 3122.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 312.2, 'scale_label': '100 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_10x_P2.tif', 'camera': 'Nikon', 'match_pattern': '10x_P2', 'pixels_per_mm': 3134.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 313.4, 'scale_label': '100 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_10x_P3.tif', 'camera': 'Nikon', 'match_pattern': '10x_P3', 'pixels_per_mm': 3126.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 312.6, 'scale_label': '100 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_5x_P1.tif', 'camera': 'Nikon', 'match_pattern': '5x_P1', 'pixels_per_mm': 1504.0, 'scale_size_mm': 0.5, 'scale_size_pixels': 752.0, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_5x_P2.tif', 'camera': 'Nikon', 'match_pattern': '5x_P2', 'pixels_per_mm': 1536.0, 'scale_size_mm': 0.5, 'scale_size_pixels': 768.0, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_5x_P3.tif', 'camera': 'Nikon', 'match_pattern': '5x_P3', 'pixels_per_mm': 1557.0, 'scale_size_mm': 0.5, 'scale_size_pixels': 778.5, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF2_P1.tif', 'camera': 'Nikon', 'match_pattern': 'CF2_P1', 'pixels_per_mm': 238.8, 'scale_size_mm': 1.0, 'scale_size_pixels': 238.8, 'scale_label': '1 mm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF2_P2.tif', 'camera': 'Nikon', 'match_pattern': 'CF2_P2', 'pixels_per_mm': 336.0, 'scale_size_mm': 1.0, 'scale_size_pixels': 336.0, 'scale_label': '1 mm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF2_P3.tif', 'camera': 'Nikon', 'match_pattern': 'CF2_P3', 'pixels_per_mm': 416.6, 'scale_size_mm': 1.0, 'scale_size_pixels': 416.6, 'scale_label': '1 mm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF3_P1.tif', 'camera': 'Nikon', 'match_pattern': 'CF3_P1', 'pixels_per_mm': 504.0, 'scale_size_mm': 1.0, 'scale_size_pixels': 504.0, 'scale_label': '1 mm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF3_P2.tif', 'camera': 'Nikon', 'match_pattern': 'CF3_P2', 'pixels_per_mm': 862.5, 'scale_size_mm': 0.5, 'scale_size_pixels': 431.25, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF3_P3.tif', 'camera': 'Nikon', 'match_pattern': 'CF3_P3', 'pixels_per_mm': 703.5, 'scale_size_mm': 0.5, 'scale_size_pixels': 351.75, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF4_P1.tif', 'camera': 'Nikon', 'match_pattern': 'CF4_P1', 'pixels_per_mm': 784.0, 'scale_size_mm': 1.0, 'scale_size_pixels': 784.0, 'scale_label': '1 mm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF4_P2.tif', 'camera': 'Nikon', 'match_pattern': 'CF4_P2', 'pixels_per_mm': 912.3333333, 'scale_size_mm': 0.5, 'scale_size_pixels': 456.1666667, 'scale_label': '500 µm'},
+ {'station': 'green', 'scale_file': 'NikonD5100_CF4_P3.tif', 'camera': 'Nikon', 'match_pattern': 'CF4_P3', 'pixels_per_mm': 1016.0, 'scale_size_mm': 0.5, 'scale_size_pixels': 508.0, 'scale_label': '500 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_5x_P1.tif', 'camera': 'Canon', 'match_pattern': '5x_P1', 'pixels_per_mm': 1092.5, 'scale_size_mm': 0.5, 'scale_size_pixels': 546.25, 'scale_label': '500 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_5x_P2.tif', 'camera': 'Canon', 'match_pattern': '5x_P2', 'pixels_per_mm': 1117.0, 'scale_size_mm': 0.5, 'scale_size_pixels': 558.5, 'scale_label': '500 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_5x_P3.tif', 'camera': 'Canon', 'match_pattern': '5x_P3', 'pixels_per_mm': 1138.666667, 'scale_size_mm': 0.5, 'scale_size_pixels': 569.3333333, 'scale_label': '500 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_10x_P1.tif', 'camera': 'Canon', 'match_pattern': '10x_P1', 'pixels_per_mm': 2280.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 228.0, 'scale_label': '100 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_10x_P2.tif', 'camera': 'Canon', 'match_pattern': '10x_P2', 'pixels_per_mm': 2281.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 228.1, 'scale_label': '100 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_10x_P3.tif', 'camera': 'Canon', 'match_pattern': '10x_P3', 'pixels_per_mm': 2287.0, 'scale_size_mm': 0.1, 'scale_size_pixels': 228.7, 'scale_label': '100 µm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF2_P1.tif', 'camera': 'Canon', 'match_pattern': 'CF2_P1', 'pixels_per_mm': 174.8, 'scale_size_mm': 1.0, 'scale_size_pixels': 174.8, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF2_P2.tif', 'camera': 'Canon', 'match_pattern': 'CF2_P2', 'pixels_per_mm': 237.3, 'scale_size_mm': 1.0, 'scale_size_pixels': 237.3, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF2_P3.tif', 'camera': 'Canon', 'match_pattern': 'CF2_P3', 'pixels_per_mm': 301.4, 'scale_size_mm': 1.0, 'scale_size_pixels': 301.4, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF3_P1.tif', 'camera': 'Canon', 'match_pattern': 'CF3_P1', 'pixels_per_mm': 277.6, 'scale_size_mm': 1.0, 'scale_size_pixels': 277.6, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF3_P2.tif', 'camera': 'Canon', 'match_pattern': 'CF3_P2', 'pixels_per_mm': 296.375, 'scale_size_mm': 1.0, 'scale_size_pixels': 296.375, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF3_P3.tif', 'camera': 'Canon', 'match_pattern': 'CF3_P3', 'pixels_per_mm': 405.6, 'scale_size_mm': 1.0, 'scale_size_pixels': 405.6, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF4_P1.tif', 'camera': 'Canon', 'match_pattern': 'CF4_P1', 'pixels_per_mm': 575.0, 'scale_size_mm': 1.0, 'scale_size_pixels': 575.0, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF4_P2.tif', 'camera': 'Canon', 'match_pattern': 'CF4_P2', 'pixels_per_mm': 654.0, 'scale_size_mm': 1.0, 'scale_size_pixels': 654.0, 'scale_label': '1 mm'},
+ {'station': 'purple', 'scale_file': 'Canon6D_CF4_P3.tif', 'camera': 'Canon', 'match_pattern': 'CF4_P3', 'pixels_per_mm': 740.3333333, 'scale_size_mm': 1.0, 'scale_size_pixels': 740.3333333, 'scale_label': '1 mm'}])
     return scale_info
 
 # Function to add a scale bar to an image
